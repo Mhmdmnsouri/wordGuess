4d52b96c0a935adde6ad6cdbf6277cd1f5232a12
Mohamad Shahmansouri <Mhmdmnsourix@gmail.com>
2022-08-30 15:12:16 +0430

    Update script.js

--- javaScript/script.js
+++ javaScript/script.js
@@ -65,7 +65,6 @@ function randomArray(arr) {
     for (let i = arr.length - 1; i > 0; i--) {
         let temp = arr[i];
         let j = Math.floor(Math.random() * (i + 1));
-
         arr[i] = arr[j];
         arr[j] = temp;
     }
